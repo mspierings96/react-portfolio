f8e07fbae8afa8eabb97d74421ca1907f867fbdb
Your Name <you@example.com>
2021-07-26 18:54:52 -0500

    intialized code

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+function Header(props) {
+  return (
+    <div>
+      <h1>Mitchell Spierings</h1>
+      <nav>
+        <div>
+          <a href="/about">About Me</a>
+        </div>
+        <div>
+          <a href="/portfolio">Portfolio</a>
+        </div>
+        <div>
+          <a href="/contact">Contact</a>
+        </div>
+        <div>
+          <a href="/resume">Resume</a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
