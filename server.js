444c28efe9aed03a6649b8a416aecd8cd874334d
Your Name <you@example.com>
2021-07-28 17:28:20 -0500

    setting up for heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+var PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("client/build"));
+
+var path = require("path");
+
+// routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
